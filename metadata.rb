b844f8a1dd458c560c5e6441ea8e63a879c09501
Tim Boudreau <tim.boudreau@rackspace.com>
2015-12-04 09:52:30 -0800

    Upgrade to Repose 7.3.0.0 and extract-device-id filter bundler 1.2.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures ele-repose'
 long_description 'Installs/Configures ele-repose'
-version '0.3.5'
+version '0.3.6'
 
 depends 'apt'
 depends 'java'
